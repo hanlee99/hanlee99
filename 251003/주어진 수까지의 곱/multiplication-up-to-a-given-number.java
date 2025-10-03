c74d50c767ca9513be6d620fa5ed2e0b8d8ad8e7
irani <93562388+hanlee99@users.noreply.github.com>
2025-10-03 21:54:51 +0900

    Title: 주어진 수까지의 곱, Time: 125ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251003/주어진 수까지의 곱/multiplication-up-to-a-given-number.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int sum=0;
+        int A=sc.nextInt();
+        int B=sc.nextInt();
+        
+        for(int i=A; i<=B; i++){
+            sum*=i;
+        }
+        System.out.println(sum);
+
+    }
+}
