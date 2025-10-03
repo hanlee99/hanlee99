d46c848f8829fb523c13dfb9207aaf9e9faf9ed4
irani <93562388+hanlee99@users.noreply.github.com>
2025-10-03 16:32:49 +0900

    Title: 시력 검사, Time: 143ms, Memory: 11MB, Status: Passed - Codetree

---
+++ 251003/시력 검사/eye-test.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        double a=sc.nextDouble();
+        double b=sc.nextDouble();
+        String res;
+        if(a>=1.0 && b>=1.0){
+            res="High";
+        }else if(a>=0.5 && b>=0.5){
+            res="Middle";
+        }else{
+            res="Low";
+        }
+        System.out.println(res);
+        
+    }
+}
