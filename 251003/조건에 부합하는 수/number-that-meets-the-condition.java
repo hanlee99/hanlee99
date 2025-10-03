f726db30b13b1ee4e6aa85e1c158b404ec05abd2
irani <93562388+hanlee99@users.noreply.github.com>
2025-10-03 22:27:31 +0900

    Title: 조건에 부합하는 수, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251003/조건에 부합하는 수/number-that-meets-the-condition.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int sum=0;
+        int A=sc.nextInt();
+        
+        for(int i=1; i<=A; i++){
+            if(i%2==0 && i%4!=0) continue;
+            if((i/8)%2==0) continue;
+            if(i%7 < 4) continue;
+            System.out.print(i + " ");
+        }
+        
+
+    }
+}
