c42999d329ed44d06142a88d1679ebfbcb60a636
irani <93562388+hanlee99@users.noreply.github.com>
2025-10-03 15:02:22 +0900

    Title: 4가지 관계연산자, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251003/4가지 관계연산자/four-relational-operators.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+
+        int res1 = a>=b ? 1 : 0;
+        int res2 = a>b ? 1 : 0;
+        int res3 = b>=a ? 1 : 0;
+        int res4 = b>a ? 1 : 0;
+        System.out.println(res1 + "\n" + res2 + "\n"+res3+"\n"+res4);
+    }
+}
