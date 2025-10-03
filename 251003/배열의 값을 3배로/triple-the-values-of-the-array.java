d70a6a2a53bcb99e15ec69f787c637447269418d
irani <93562388+hanlee99@users.noreply.github.com>
2025-10-03 10:45:34 +0900

    Title: 배열의 값을 3배로, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251003/배열의 값을 3배로/triple-the-values-of-the-array.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int[][] arr = new int[3][3];
+
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                int n = sc.nextInt();
+                arr[i][j] = n*3;
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
