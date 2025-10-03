569244e5766430fd39892dec94567718517b4524
irani <93562388+hanlee99@users.noreply.github.com>
2025-10-03 22:31:03 +0900

    Title: 1부터 곱셈하기, Time: 134ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251003/1부터 곱셈하기/multiple-from-one.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int sum=1;
+        int N=sc.nextInt();
+        
+        for(int i=1; i<=N; i++){
+            sum*=i;
+            if(sum>=N){
+                System.out.println(i);
+                break;
+            }
+        }
+
+    }
+}
