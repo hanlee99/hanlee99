54a93b02f252599c26310e0f923fdf9e0200aebb
irani <93562388+hanlee99@users.noreply.github.com>
2025-10-03 14:40:14 +0900

    Title: 살 수 있는 물건 2, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251003/살 수 있는 물건 2/things-able-to-buy-2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        String res;
+        if(a>=3000){
+            res="book";
+        }else if(a>=1000){
+            res="mask";
+        }else if(a>=500){
+            res="pen";
+        }else{
+            "no";
+        }
+        
+        System.out.println(res);
+    }
+}
