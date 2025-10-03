517720cef20095189e78d24a2105deed4dd3f225
irani <93562388+hanlee99@users.noreply.github.com>
2025-10-03 09:53:09 +0900

    Title: 특정 수 맞히기, Time: 121ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251003/특정 수 맞히기/catching-specific-number.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int com=0;
+        while(sc.hasNext()){
+            com  = sc.nextInt();
+            if(com > 25){
+                System.out.println("Lower");
+            }else if(com == 25){
+                System.out.println("Good");
+                break;
+            }else{
+                System.out.println("Higher");
+            }
+        }
+
+    }
+}
