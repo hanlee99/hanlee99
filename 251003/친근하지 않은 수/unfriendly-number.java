3af0c084dfdc62179ecc1e735e53fa063d6bb216
irani <93562388+hanlee99@users.noreply.github.com>
2025-10-03 22:02:08 +0900

    Title: 친근하지 않은 수, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

--- 251003/친근하지 않은 수/unfriendly-number.java
+++ 251003/친근하지 않은 수/unfriendly-number.java
@@ -4,12 +4,14 @@ public class Main {
         // Please write your code here.
         Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
-        int count=0;
+        int cnt=0;
+        int N=sc.nextInt();
+        
         for(int i=1; i<=N; i++){
-            if(i%2!=0 && i%3!=0 && i%5!=0)
-                count++;
+            if(!(i%2==0 || i%3==0 || i%5==0))
+                cnt++;
         }
-        System.out.println(count);
+        System.out.println(cnt);
+
     }
 }
