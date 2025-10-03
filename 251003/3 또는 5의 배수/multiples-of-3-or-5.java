0304218b3368e2ddbd38441bead63290efca2599
irani <93562388+hanlee99@users.noreply.github.com>
2025-10-03 14:53:52 +0900

    Title: 3 또는 5의 배수, Time: 122ms, Memory: 10MB, Status: Passed - Codetree

--- 251003/3 또는 5의 배수/multiples-of-3-or-5.java
+++ 251003/3 또는 5의 배수/multiples-of-3-or-5.java
@@ -4,8 +4,8 @@ public class Main {
         // Please write your code here.
         Scanner sc = new Scanner(System.in);
         int a=sc.nextInt();
-        String res1 = a%3==0 ? "Yes" : "No";
-        String res2 = a%5==0 ? "Yes" : "No";
+        String res1 = a%3==0 ? "YES" : "NO";
+        String res2 = a%5==0 ? "YES" : "NO";
         System.out.println(res1 + "\n" + res2);
     }
 }
