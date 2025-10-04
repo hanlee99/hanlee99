5bf492dba10e7daf33517ca87ce41ac4cc77b0ae
irani <93562388+hanlee99@users.noreply.github.com>
2025-10-04 18:24:26 +0900

    Title: 원소 10개의 합, Time: 135ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251004/원소 10개의 합/sum-of-10-elements.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int sum=0;
+        while(sc.hasNext()){
+            int a=sc.nextInt();
+            sum+=a;
+        }
+        System.out.println(sum);
+    }
+}
