f98b631c8e35607bf810a8844ce15109d0a9fa68
irani <93562388+hanlee99@users.noreply.github.com>
2025-10-05 13:14:31 +0900

    Title: 두 번째를 첫 번째로, Time: 113ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251005/두 번째를 첫 번째로/second-to-first.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String word=sc.next();
+        char one=word.charAt(0);
+        char two=word.charAt(1);
+
+        char[] arr = word.toCharArray();
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] == two) 
+                arr[i]=one;
+        }
+        System.out.println(String.valueOf(arr));
+    }
+}
