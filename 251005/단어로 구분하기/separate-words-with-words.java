7b7cfc031717237fe2d5ea70bcc7e3a66d48577f
irani <93562388+hanlee99@users.noreply.github.com>
2025-10-05 11:50:43 +0900

    Title: 단어로 구분하기, Time: 120ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251005/단어로 구분하기/separate-words-with-words.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int res=0;
+        while(sc.hasNext()){
+            System.out.print(sc.next() + " ");
+        }
+        
+    }
+}
