16a8df4e2aa863f11edb8527550f6039c0c017d0
irani <93562388+hanlee99@users.noreply.github.com>
2025-10-06 16:19:29 +0900

    Title: 미는 횟수, Time: 116ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251006/미는 횟수/number-of-pushes.java
+++ 251006/미는 횟수/number-of-pushes.java
@@ -3,28 +3,24 @@ public class Main {
     public static void main(String[] args) {
         // Please write your code here.
         Scanner sc = new Scanner(System.in);
-        int cnt=0;
-        int answer=-1;
-        boolean flag=false;
+        int cnt=-1;
         char[] A=sc.next().toCharArray();
         char[] B=sc.next().toCharArray();
 
         for(int i=1; i<A.length; i++){
-            cnt++;
-            int j=0;
-            for(char c : B){
-                if(c != A[(i+j)%A.length]){
-                    j=0;
+            boolean same=true;
+            for(int j=0; j<B.length; j++){
+                if(B[j] != A[(i+j)%A.length]){
+                    same=false;
                     break;
                 }
-                j++;
             }
-            if(j==A.length) {
-                answer=cnt;
+            if(same) {
+                cnt=i;
                 break;
             }
         }
         
-        System.out.println(answer);
+        System.out.println(cnt);
     }
 }
