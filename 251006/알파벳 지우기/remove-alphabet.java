a3a706675c6cdc29fde31b0b0d5da509698058f9
irani <93562388+hanlee99@users.noreply.github.com>
2025-10-06 15:23:24 +0900

    Title: 알파벳 지우기, Time: 117ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251006/알파벳 지우기/remove-alphabet.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int sum=0;
+
+        for(int i=0; i<2; i++){
+            String st=sc.next();
+            StringBuilder sb = new StringBuilder();
+            for(char c : st.toCharArray()){
+                if(c>='0'&&c<='9'){
+                    sb.append(c);
+                }
+            }
+            sum+=Integer.parseInt(sb.toString());
+        }
+
+        System.out.println(sum);
+        
+    }
+}
