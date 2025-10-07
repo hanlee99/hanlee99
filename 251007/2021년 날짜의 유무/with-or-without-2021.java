141366609f3ff861a2abdcd987c2a1c1e6f66c28
irani <93562388+hanlee99@users.noreply.github.com>
2025-10-07 14:16:50 +0900

    Title: 2021년 날짜의 유무, Time: 126ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 251007/2021년 날짜의 유무/with-or-without-2021.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int d = sc.nextInt();
+        // Please write your code here.
+        String res = isDate(m,d) ? "Yes" : "No";
+        System.out.println(res);
+    }
+    private static boolean isDate(int m, int d){
+        int[] month = {31,28,31,30,31,30,31,31,30,31,30,31};
+        
+        return month[m-1]>=d ? true : false;
+        
+    }
+}
