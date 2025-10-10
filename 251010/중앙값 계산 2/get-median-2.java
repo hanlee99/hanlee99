dddaff9f0377bd3e28c1fe9f92d305294553c221
irani <93562388+hanlee99@users.noreply.github.com>
2025-10-10 14:32:00 +0900

    Title: 중앙값 계산 2, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251010/중앙값 계산 2/get-median-2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        // Please write your code here.
+
+        for(int i=0; i<arr.length; i+=2){
+            Arrays.sort(arr,0, i+1);
+            
+            System.out.print(arr[i/2]+" ");                   
+        }
+    }
+}
