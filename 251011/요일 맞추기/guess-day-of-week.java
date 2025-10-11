3475a3a0a83561475b1167c35bfb974e3ccc4f7a
irani <93562388+hanlee99@users.noreply.github.com>
2025-10-11 17:49:18 +0900

    Title: 요일 맞추기, Time: 123ms, Memory: 10MB, Status: Runtime Error - Codetree

--- 251011/요일 맞추기/guess-day-of-week.java
+++ 251011/요일 맞추기/guess-day-of-week.java
@@ -9,7 +9,7 @@ public class Main {
         // Please write your code here.
         int[] month={0,31,28,31,30,31,30,31,31,30,31,30,31};
         String[] day = {"Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
-        int sumDay1 = d1-1;
+        int sumDay1 = d1;
         int sumDay2= d2;
         for(int i=1; i<m1; i++){
             sumDay1+=month[i];
@@ -17,7 +17,7 @@ public class Main {
         for(int i=1; i<m2; i++){
             sumDay2+=month[i];
         }
-        int diff = sumDay2-sumDay1;
+        int diff = sumDay2-sumDay1+1;
         String res = day[diff%7];
         System.out.println(res);
     }
