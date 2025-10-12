299abde3808c9b8dd6cefa41c6489a4cf41546b0
irani <93562388+hanlee99@users.noreply.github.com>
2025-10-12 18:30:36 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 115ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251012/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
+++ 251012/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -14,17 +14,16 @@ public class Main {
         Rect rect1 = new Rect(rect1_x1+n, rect1_x2+n, rect1_y1+n, rect1_y2+n);
         Rect rect2 = new Rect(rect2_x1+n, rect2_x2+n, rect2_y1+n, rect2_y2+n);
 
-        
-        int area = (rect1.x2-rect1.x1)*(rect1.y2-rect1.y1);
-        if(rect1.x1==rect2.x1&&rect1.x2==rect2.x2&&
-           rect1.y1==rect2.y1&&rect1.y2==rect2.y2){
+        int area = rect1.area();
+
+        if(rect1.contains(rect2)){
             area = 0;
-        }else if(rect1.x1>=rect2.x1&&rect1.x2<=rect2.x2){
-            int y = rect1.y1<rect2.y1 ? rect2.y1-rect1.y1 : rect1.y2-rect2.y2;
-            area = (rect1.x2-rect1.x1) * y;
+        }else if(rect1.x1>=rect2.x1 && rect1.x2<=rect2.x2){
+            int y = Math.min(rect1.y2,rect2.y2) - Math.max(rect1.y1, rect2.y1);
+            area -= (rect1.x2-rect1.x1) * y;
         }else if(rect1.y1>=rect2.y1&&rect1.y2<=rect2.y2){
-            int x = rect1.x1<rect2.x1 ? rect2.x1-rect1.x1 : rect1.x2-rect2.x2;
-            area = (rect1.y2-rect1.y1) * x;
+            int x = Math.min(rect1.x2,rect2.x2) - Math.max(rect1.x1,rect2.x1);
+            area -= (rect1.y2-rect1.y1) * x;
         }
         System.out.println(area);
     }
@@ -38,5 +37,9 @@ class Rect{
         this.x2 = Math.max(x1,x2);
         this.y1 = Math.min(y1,y2);
         this.y2 = Math.max(y1,y2);
+    }
+    public int area() { return (x2-x1) * (y2-y1); }
+    boolean contains(Rect r){
+        return x1>=r.x1 && x2<=r.x2 && y1>=r.y1 && y2<=r.y2;
     }
 }
