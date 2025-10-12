8a6aa9e0649b2c8eec9b306be90cf3a0a8dadbd8
irani <93562388+hanlee99@users.noreply.github.com>
2025-10-12 13:47:29 +0900

    Title: 진수 to 진수, Time: 127ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251012/진수 to 진수/transformation-of-number-system.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        String N = sc.next();
+        // Please write your code here.
+        int Adec = toDec(A, N);
+        String res = toBase(B, Adec);
+        System.out.println(res);
+
+    }
+    private static int toDec(int A, String N){
+        int n=0;
+        for(int i=0; i<N.length(); i++){
+            int k = N.charAt(i)=='1' ? 1 : 0;
+            n = n*A + k;
+        }
+        return n;
+    }
+    private static String toBase(int A, int n){
+        StringBuilder sb = new StringBuilder();
+        do{
+            sb.append(n%A);
+            n/=A;
+        }while(n>0);
+        return sb.reverse().toString();
+    }
+}
