648d0ea77ed3cf1331625c25e872cf802e0b305a
irani <93562388+hanlee99@users.noreply.github.com>
2025-10-12 20:42:56 +0900

    Title: 선두를 지켜라, Time: 171ms, Memory: 11MB, Status: Wrong Answer - Codetree

--- 251012/선두를 지켜라/keep-the-lead.java
+++ 251012/선두를 지켜라/keep-the-lead.java
@@ -6,53 +6,50 @@ public class Main {
         int n = sc.nextInt();
         int m = sc.nextInt();
         int[][] A = new int[n][2];
-        int dist=0; int dist2=0;
+        int timeA=0; int timeB=0;
         for (int i = 0; i < n; i++) {
             A[i][0] = sc.nextInt();
             A[i][1] = sc.nextInt();
-            dist += A[i][0] * A[i][1];
+            timeA += A[i][1];
         }
         int[][] B = new int[m][2];
         for (int i = 0; i < m; i++) {
             B[i][0] = sc.nextInt();
             B[i][1] = sc.nextInt();
-            dist2 += B[i][0] * B[i][1];
+            timeB += B[i][1];
         }
         // Please write your code here.
         char head='0';
         int cnt=0;
-        int[] AtimeLine = new int[dist+1];
-        int[] BtimeLine = new int[dist+1];
+        int[] AtimeLine = new int[timeA+1];
+        int[] BtimeLine = new int[timeB+1];
         int idx=1;
         for(int i=0; i<n; i++){
-            int u=A[i][0];
-            int t=A[i][1];
+            int u=A[i][0];  int t=A[i][1];
             for(int j=0; j<t; j++){
                 AtimeLine[idx] = AtimeLine[idx-1]+u;
                 idx++;
             }
         }
+
         idx=1;
         for(int i=0; i<m; i++){
-            int u=B[i][0];
-            int t=B[i][1];
+            int u=B[i][0];  int t=B[i][1];
             for(int j=0; j<t; j++){
                 BtimeLine[idx] = BtimeLine[idx-1]+u;
                 idx++;
             }
         }
-        for(int i=1; i<=dist; i++){
-            if(AtimeLine[i]>BtimeLine[i]){
-                if(head!='A'){
-                    cnt++;
-                    head='B';
-                }
-            }else if(AtimeLine[i]<BtimeLine[i]){
-                if(head!='B'){
-                    cnt++;
-                    head='B';
-                }
-            }
+
+        for(int i=1; i<=timeA; i++){
+            char h;
+            if(AtimeLine[i]>BtimeLine[i]) h='A';
+            if(AtimeLine[i]<BtimeLine[i]) h='B';
+            else h='0';
+
+            if(head!=h && h!='0') cnt++;
+            head=h;
+            //System.out.println(AtimeLine[i]+" "+BtimeLine[i]+" "+head+" "+cnt);
         }
         System.out.println(cnt);
     }
