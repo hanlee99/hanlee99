89a2bf36c09f06a6a4e345a38171dd3fa0ac6d13
irani <93562388+hanlee99@users.noreply.github.com>
2025-10-12 21:43:06 +0900

    Title: 좌우로 움직이는 로봇, Time: 254ms, Memory: 21MB, Status: Wrong Answer - Codetree

---
+++ 251012/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int aTotalTime=0;    int bTotalTime=0;
+        int[] aTime=new int[n];   int[] bTime=new int[m];
+        char[] aDir=new char[n];    char[] bDir=new char[m];
+
+        for (int i = 0; i < n; i++) {
+            aTime[i] = sc.nextInt();
+            aDir[i] = sc.next().charAt(0);
+            // Please write your code here.
+            aTotalTime+=aTime[i];
+        }
+        
+        for (int i = 0; i < m; i++) {
+            bTime[i] = sc.nextInt();
+            bDir[i] = sc.next().charAt(0);
+            // Please write your code here.
+            bTotalTime += bTime[i];
+        }
+        
+        int[] aTimeLine = new int[aTotalTime+1];
+        int[] bTimeLine = new int[bTotalTime+1];
+        int idx=1;
+        for(int i=0; i<n; i++){
+            int sign = aDir[i]=='R' ? 1 : -1;
+            int t = aTime[i];
+            for(int j=0; j<t; j++){
+                aTimeLine[idx] = aTimeLine[idx-1]+sign;
+                idx++;
+            }
+        }
+
+        idx=1;
+        for(int i=0; i<m; i++){
+            int sign = bDir[i]=='R' ? 1 : -1;
+            int t = bTime[i];
+            for(int j=0; j<t; j++){
+                bTimeLine[idx] = bTimeLine[idx-1]+sign;
+                idx++;
+            }
+        }
+        int cnt=0;
+        boolean visited = false;
+        int max=Math.max(aTotalTime,bTotalTime);
+        int min=Math.min(aTotalTime,bTotalTime);
+        for(int i=1; i<=min; i++){
+            if(aTimeLine[i]==bTimeLine[i]){
+                if(!visited){
+                    cnt++;
+                    visited=true;
+                }
+            }else  visited=false;
+            //System.out.println("<"+i+"> "+aTimeLine[i]+" "+bTimeLine[i]+" "+cnt+" ");
+        }
+        //System.out.println("\n");
+        int[] timeLine = aTotalTime>bTotalTime ? aTimeLine : bTimeLine;
+        int target=aTotalTime>bTotalTime ? bTimeLine[bTotalTime] : aTimeLine[aTotalTime];
+        //System.out.println(target+"\n");
+        for(int i=min+1; i<=max; i++){
+            if(target == timeLine[i]){
+                if(!visited){
+                    cnt++;
+                    visited=true;
+                }
+            }else  visited=false;
+            //System.out.println("<"+i+"> "+timeLine[i]+" "+cnt);
+        }
+        System.out.println(cnt);
+        // Please write your code here.
+    }
+}
