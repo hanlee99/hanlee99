a96d4d5de85bcbb032a9f9efe3ef4ba44215572e
irani <93562388+hanlee99@users.noreply.github.com>
2025-10-12 19:51:37 +0900

    Title: 만나는 그 순간, Time: 126ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251012/만나는 그 순간/the-moment-we-meet.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] Atime = new int[n];
+        char[] direction = new char[n];
+        int sum=0;
+        for(int i=0; i<n; i++){
+            direction[i] = sc.next().charAt(0);
+            Atime[i] = sc.nextInt();
+            sum+=direction[i];
+        }
+        // Please write your code here.
+        int[] AtimeLine = new int[sum+1];
+        int[] BtimeLine = new int[sum+1];
+        int idx=1;
+        for(int i=0; i<n; i++){
+            int sign = direction[i] == 'R' ? 1 : -1;
+            int time = Atime[i];
+            for(int j=0; j<time; j++){
+                AtimeLine[idx] = AtimeLine[idx-1]+sign;
+                idx++;
+            }
+        }
+        idx=1;
+        for(int i=0; i<m; i++){
+            int sign = sc.next().charAt(0)=='R' ? 1 : -1;
+            int time = sc.nextInt();
+
+            for(int j=0; j<time; j++){
+                BtimeLine[idx] = BtimeLine[idx-1]+sign;
+                idx++;
+            }
+        }
+        int res=-1;
+        for(int i=1; i<sum; i++){
+            if(AtimeLine[i] == BtimeLine[i]){
+                res=i;
+                break;
+            }
+        }
+        System.out.println(res);
+
+    }
+}
