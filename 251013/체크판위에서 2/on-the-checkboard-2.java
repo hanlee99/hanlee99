de973556b495dbb4d7c5454da935cc144b6505d2
irani <93562388+hanlee99@users.noreply.github.com>
2025-10-13 21:45:39 +0900

    Title: 체크판위에서 2, Time: 147ms, Memory: 10MB, Status: Passed - Codetree

--- 251013/체크판위에서 2/on-the-checkboard-2.java
+++ 251013/체크판위에서 2/on-the-checkboard-2.java
@@ -1,34 +1,35 @@
 import java.util.Scanner;
+
 public class Main {
-    static char[] arr;
+    public static final int MAX_N = 15;
+
+    public static int n, m;
+    public static char[][] grid = new char[MAX_N][MAX_N];
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int R = sc.nextInt();
-        int C = sc.nextInt();
-        char[][] grid = new char[R][C];
-        for (int i = 0; i < R; i++) {
-            for (int j = 0; j < C; j++) {
+
+        // 입력
+        n = sc.nextInt();
+        m = sc.nextInt();
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++) {
                 grid[i][j] = sc.next().charAt(0);
             }
-        }
-        // Please write your code here.
-        int cnt=0;
-        
-        for(int i=1; i<R; i++){
-            for(int j=1; j<C; j++){
-                char cur = grid[0][0];
 
-                if(grid[i][j] == cur) continue;
-                cur=grid[i][j];
-                for(int a=i+1; a<R-1; a++){
-                    for(int b=j+1; b<C-1; b++){
-                        if(cur != grid[a][b]) cnt++;
-                    }
-                }
-                
-            }
-        }
+        // 이동 시에 행과 열이 전부 증가하도록
+        // 모든 쌍을 다 잡아봅니다.
+        int cnt = 0;
+        for(int i = 1; i < n; i++)
+            for(int j = 1; j < m; j++)
+                for(int k = i + 1; k < n - 1; k++)
+                    for(int l = j + 1; l < m - 1; l++)
+                        // 그 중 색깔이 전부 달라지는 경우에만 개수를 세줍니다.
+                        if(grid[0][0] != grid[i][j] && 
+                           grid[i][j] != grid[k][l] &&
+                           grid[k][l] != grid[n - 1][m - 1])
+                            cnt++;
+        
         System.out.println(cnt);
     }
-    
 }
