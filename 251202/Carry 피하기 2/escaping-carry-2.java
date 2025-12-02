788fd50fdc5f10a8f06f4e40a1aaba5e693c7b59
irangi <93562388+hanlee99@users.noreply.github.com>
2025-12-02 16:11:42 +0900

    Title: Carry 피하기 2, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

--- 251202/Carry 피하기 2/escaping-carry-2.java
+++ 251202/Carry 피하기 2/escaping-carry-2.java
@@ -10,34 +10,25 @@ public class Main {
         // Please write your code here.
         
         int largest = -1;
-        int[][] num = new int[n][5];
-        for(int i=0; i<n; i++){
-            int k=arr[i];
-            int j=0;
-            while(j<5){
-                num[i][j] = k%10;
-                k/=10;
-                j++;
-            }
-        }
 
         for(int i=0; i<n-2; i++){
-            
             for(int j=i+1; j<n-1; j++){
                 for(int k=j+1; k<n; k++){
                     //System.out.println(arr[i]+" "+arr[j]+" "+arr[k]);
                     boolean flag = true;
-                    int m=0;
-                    while(m<5){
-                        if(num[i][m]+num[j][m]+num[k][m] >9){
+                    int a=arr[i];   int b=arr[j];   int c=arr[k];
+
+                    for(int m=0; m<5; m++){
+                        if( (a%10 + b%10 + c%10) > 9){
                             flag=false;
                             break;
                         }
-                        m++;
+                        a/=10;  b/=10;  c/=10;
                     }
                     if(flag){
                         largest = Math.max(largest, arr[i]+arr[j]+arr[k]);
                     }
+
                 }
             }
         }
