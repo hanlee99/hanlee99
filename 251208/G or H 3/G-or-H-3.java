d40e08bc46249ba720266b5dc144a5371034fb2b
irangi <93562388+hanlee99@users.noreply.github.com>
2025-12-08 12:06:12 +0900

    Title: G or H 3, Time: 129ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251208/G or H 3/G-or-H-3.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] point = new int[n];
+        int[] score = new int[n];
+        int max=0;
+        for (int i = 0; i < n; i++) {
+            int pos = sc.nextInt();
+            char c = sc.next().charAt(0);
+            max = Math.max(max, pos);
+            point[i] = pos;
+            if(c=='G') score[i]=1;
+            if(c=='H') score[i]=2;
+        }
+        // Please write your code here.
+
+        int[] lines = new int[max+1];
+        for(int i=0; i<n; i++){
+            lines[point[i]] = score[i];
+        }
+        int res=0;
+        for(int i=1; i<max+1-k; i++){
+            int maxPoint=0;
+            for(int j=0; j<=k; j++){
+                maxPoint+=lines[i+j];
+            }
+            res = Math.max(res, maxPoint);
+            //System.out.println(res+" "+maxPoint);
+
+        }
+        System.out.println(res);
+
+    }
+}
