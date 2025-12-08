0df7b9ff65ddf17f2016279accb71d521f8c2ade
irangi <93562388+hanlee99@users.noreply.github.com>
2025-12-08 19:45:35 +0900

    Title: 데이터센터의 온도 조정 2, Time: 233ms, Memory: 14MB, Status: Wrong Answer - Codetree

--- 251208/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.java
+++ 251208/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.java
@@ -18,7 +18,7 @@ public class Main {
         }
         // Please write your code here.
         int work=0;
-        for(int i=min-1; i<=max+1; i++){
+        for(int i=0; i<=1000; i++){
             int curWork=0;
             for(int j=0; j<n; j++){
                 if(i<ta[j]){
