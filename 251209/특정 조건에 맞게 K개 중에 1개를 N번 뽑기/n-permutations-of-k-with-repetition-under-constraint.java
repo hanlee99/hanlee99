092de16ea89b22716d1ac7432859449e06431430
irangi <93562388+hanlee99@users.noreply.github.com>
2025-12-09 17:47:35 +0900

    Title: 특정 조건에 맞게 K개 중에 1개를 N번 뽑기, Time: 1121ms, Memory: 24MB, Status: Passed - Codetree

--- 251209/특정 조건에 맞게 K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition-under-constraint.java
+++ 251209/특정 조건에 맞게 K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition-under-constraint.java
@@ -1,32 +1,42 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+
 public class Main {
-    static int k;
-    static int n;
-    static ArrayList<Integer> list = new ArrayList<>();
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        k = sc.nextInt();
-        n = sc.nextInt();
-        // Please write your code here.
-        dfs(0);
+    public static int k, n;
+    public static ArrayList<Integer> selectedNums = new ArrayList<>();
+    
+    // 선택된 원소들을 출력해줍니다.
+    public static void printPermutation() {
+        for(int i = 0; i < selectedNums.size(); i++)
+            System.out.print(selectedNums.get(i) + " ");
+        System.out.println();
     }
-    private static void dfs(int depth){
-        if(depth == n){
-            for(int i=0; i<list.size(); i++){
-                System.out.print(list.get(i));
-                if(i!=list.size()-1) System.out.print(" ");
-            }
-            System.out.println();
+
+    public static void findPermutations(int cnt) {
+        // n개를 모두 뽑은 경우 답을 출력해줍니다.
+        if(cnt == n) {
+            printPermutation();
             return;
         }
-        
-        for(int i=1; i<=k; i++){
-            if(list.size()>=2 && list.get(list.size()-1) == i 
-                            && list.get(list.size()-2) == i) continue;
-            list.add(i);
-            dfs(depth+1);
-            list.remove(list.size()-1);
+
+        for(int i = 1; i <= k; i++) {
+            if(cnt >= 2 && 
+               i == selectedNums.get(selectedNums.size() - 1) && 
+               i == selectedNums.get(selectedNums.size() - 2))
+               continue;
+
+            selectedNums.add(i);
+            findPermutations(cnt + 1);
+            selectedNums.remove(selectedNums.size() - 1);
         }
     }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        k = sc.nextInt();
+        n = sc.nextInt();
+        
+		findPermutations(0);
+	}
 }
