297c5d22228cb24e9cef7405aa45e96446ae7247
irangi <93562388+hanlee99@users.noreply.github.com>
2025-12-09 17:03:54 +0900

    Title: K개 중에 1개를 N번 뽑기, Time: 702ms, Memory: 26MB, Status: Passed - Codetree

--- 251209/K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition.java
+++ 251209/K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition.java
@@ -1,32 +1,21 @@
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class Main {
-    static int k;
-    static int n;
-    static ArrayList<Integer> list = new ArrayList<>();
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        k = sc.nextInt();
-        n = sc.nextInt();
+        int k = sc.nextInt();
+        int n = sc.nextInt();
         // Please write your code here.
-        dfs(0);
+        dfs(k, n, 0, "");
     }
 
-    private static void dfs(int depth){
-        if(depth == n){
-            for(int i=0; i<list.size(); i++){
-                System.out.print(list.get(i));
-                if(i<list.size()-1) System.out.print(" ");
-            }
-            System.out.println();
+    private static void dfs(int k, int n,int depth, String st){
+        if(depth >= n){
+            System.out.println(st.trim());
             return;
         }
-
         for(int i=1; i<=k; i++){
-            list.add(i);
-            dfs(depth + 1);
-            list.remove(list.size()-1);
+            dfs(k, n, depth+1, st+" "+i);
         }
     }
 }
