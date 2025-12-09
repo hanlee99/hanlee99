45713ddb3ec450729a73c241c1aa575864253f3f
irangi <93562388+hanlee99@users.noreply.github.com>
2025-12-09 14:37:48 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 311ms, Memory: 21MB, Status: Passed - Codetree

---
+++ 251209/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+public class Main {
+    static final int[] dx = {1, 0, -1, 0};
+    static final int[] dy = {0, -1, 0, 1};
+    static int[][] grid;
+    static boolean[][] visited;
+    static ArrayDeque<int[]> queue = new ArrayDeque<>();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        grid = new int[n][m];
+        queue.add(new int[]{0, 0});
+        visited = new boolean[n][m];
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                grid[i][j] = sc.nextInt();
+        // Please write your code here.
+        while(!queue.isEmpty()){
+            int[] curr = queue.poll();
+
+            for(int dir=0; dir<4; dir++){
+                int nx=curr[0]+dx[dir];
+                int ny=curr[1]+dy[dir];
+                
+                if(isRange(nx, ny) && 
+                   grid[nx][ny]==1 && !visited[nx][ny]){
+                    queue.offer(new int[]{nx,ny});
+                    visited[nx][ny] = true;
+                }
+            }
+        }
+
+        int answer = visited[n-1][m-1] ? 1 : 0;
+        System.out.println(answer);
+    }
+    private static boolean isRange(int x, int y){
+        return (x>=0 && x<grid.length && 
+                y>=0 && y<grid[x].length);
+    }
+}
