7b86975040a1e1023abe50dc7b79a4ff7d452f74
irangi <93562388+hanlee99@users.noreply.github.com>
2025-12-09 18:05:07 +0900

    Title: N개 중에 M개 뽑기, Time: 137ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251209/N개 중에 M개 뽑기/n-choose-m.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+    static int n;
+    static int m;
+    static ArrayList<Integer> list = new ArrayList<>();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        // Please write your code here.
+        dfs(0, 1);
+    }
+    private static void dfs(int depth, int curr){
+        if(depth == m){
+            for(int i=0; i<list.size(); i++){
+                System.out.print(list.get(i));
+                if(list.size()-1!=i) System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+        for(int i=curr; i<=n; i++){
+            list.add(i);
+            dfs(depth+1,i+1);
+            list.remove(list.size()-1);
+        }
+    }
+
+}
