dfd213ffb7e55624d539f096ce4afb7f31c31fd4
irangi <93562388+hanlee99@users.noreply.github.com>
2025-12-09 17:41:05 +0900

    Title: 특정 조건에 맞게 K개 중에 1개를 N번 뽑기, Time: 1609ms, Memory: 24MB, Status: Passed - Codetree

--- 251209/특정 조건에 맞게 K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition-under-constraint.java
+++ 251209/특정 조건에 맞게 K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition-under-constraint.java
@@ -3,13 +3,11 @@ import java.util.ArrayList;
 public class Main {
     static int k;
     static int n;
-    static int[] cnt;
     static ArrayList<Integer> list = new ArrayList<>();
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         k = sc.nextInt();
         n = sc.nextInt();
-        cnt = new int[k+1];
         // Please write your code here.
         dfs(0);
     }
@@ -20,15 +18,15 @@ public class Main {
                 if(i!=list.size()-1) System.out.print(" ");
             }
             System.out.println();
+            return;
         }
         
         for(int i=1; i<=k; i++){
-            if(cnt[i]>=2) continue;
+            if(list.size()>=2 && list.get(list.size()-1) == i 
+                            && list.get(list.size()-2) == i) continue;
             list.add(i);
-            cnt[i]++;
             dfs(depth+1);
             list.remove(list.size()-1);
-            cnt[i]--;
         }
     }
 }
